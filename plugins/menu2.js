5b7c6d1fadee5034800f56784cdaf748bfb9658e
Kaisen-sudo <player711025@gmail.com>
2025-03-20 15:31:08 -0400

    Add files via upload

---
+++ plugins/menu2.js
@@ -0,0 +1,107 @@
+/*created by ᴋᴀɪsᴇɴ 🕵
+contact dev1 50941091397♻️
+contact dev2 https://t.me/kaisensudo ♻️
+© just give credit will you ⚠
+*/
+
+
+
+
+
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "menu2",
+    react: "👾",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `*┋* ${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = `*╭─────────────────❒⁠⁠⁠⁠*
+
+*⇆ ʜɪ ᴍʏ ᴅᴇᴀʀ ғʀɪᴇɴᴅ ⇆*
+
+     *${pushname}*
+
+┏━━━━━━━━━━━━━━━━━━━━
+   *WELCOME TO KAISEN-MD 2ND MENU*
+┗━━━━━━━━━━━━━━━━━━━━
+
+*ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴋᴀɪsᴇɴ 👨🏻‍💻*
+
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂ DOWNLOAD CMDS❂*
+*┕───────────────❒*
+*╭──────────●●►*
+${menu.download}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂MAIN CMDS❂*
+*┕───────────────❒*
+*╭──────────●●►*
+${menu.main}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂GROUP CMDS❂*
+*┕───────────────❒*
+
+*╭──────────●●►*
+${menu.group}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂OWNER CMDS❂*
+*┕───────────────❒*
+
+*╭──────────●●►*
+${menu.owner}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂CONVERTER CMDS❂*
+*┕───────────────❒*
+
+*╭──────────●●►*
+${menu.convert}
+*╰──────────●●►*
+
+*╭─────────────────❒⁠⁠⁠⁠*
+*│* *❂SEARCH CMDS ❂*
+*┕─────────────────❒*
+
+*╭──────────●●►*
+${menu.search}
+*╰──────────●●►*
+
+
+> *POWERED BY KAISEN-MD*
+
+`;
+
+await conn.sendMessage(from,{image:{url:config.MENU_IMG},caption:madeMenu},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
